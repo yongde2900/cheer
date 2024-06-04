981a5ffbfd4f583b1a191e3caacf2dd3fb4210a6
yongde <com414141@gmail.com>
2024-06-04 15:57:26 +0800

    refactor: refactor response object types across the codebase
    - Add types for SuccessResponse and ErrorResponse objects

---
+++ src/core/types/index.ts
@@ -0,0 +1,12 @@
+import { ValidationType } from "../errors";
+
+export interface SuccessResponse<T> {
+	data?: T;
+}
+
+export interface ErrorResponse {
+  name: string;
+  message: string;
+  ValidationErrors?: ValidationType;
+  stack?: string;
+}
