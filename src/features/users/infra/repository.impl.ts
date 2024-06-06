29ca67eca2308614eb478a4feb50e4af3cdf489d
yongde <com414141@gmail.com>
2024-06-06 13:42:04 +0800

    feat: implement User Repository with CRUD operations
    - Add a new file `index.ts` in the `src/features/users/infra` directory
    - Add a new file `repository.impl.ts` in the `src/features/users/infra` directory
    - Implement the `UserRepository` class in `repository.impl.ts` with methods for create, delete, getById, getAll, getByEmail, and edit operations

---
+++ src/features/users/infra/repository.impl.ts
@@ -0,0 +1,56 @@
+import { AppError } from '../../../core';
+import { CreateUserDto, EditUserDto, GetAllUserDto, UserEntity, UserRepository } from '../domain';
+import { PqUserDataSource } from '../domain/dataSource/pq.dataSource';
+import { RedisUserDataSource } from '../domain/dataSource/redis.dataSource';
+
+export class UserRepositoryImpl implements UserRepository {
+	constructor(
+		private readonly pqDataSource: PqUserDataSource,
+		private readonly redisDataSource: RedisUserDataSource
+	) {}
+
+	async create(createDto: CreateUserDto): Promise<UserEntity> {
+		const user = await this.pqDataSource.create(createDto);
+		await this.redisDataSource.invalidateListCache();
+		return user;
+	}
+
+	async delete(id: number): Promise<void> {
+		await this.pqDataSource.delete(id);
+		await this.redisDataSource.invalidateUserCache(id);
+	}
+
+	async getById(id: number): Promise<UserEntity> {
+		let user: UserEntity | null = await this.redisDataSource.getById(id);
+		if (user) {
+			return user;
+		}
+
+		user = await this.pqDataSource.getById(id);
+		if (!user) {
+			throw AppError.notFound('User not found');
+		}
+
+		await this.redisDataSource.setUser(user);
+		return user;
+	}
+
+	async getAll(getAllUserDto: GetAllUserDto): Promise<UserEntity[]> {
+		let users = await this.redisDataSource.getAll(getAllUserDto);
+		if (!users) {
+			users = await this.pqDataSource.getAll(getAllUserDto);
+			await this.redisDataSource.setAll(getAllUserDto, users);
+		}
+		return users;
+	}
+
+	async getByEmail(email: string): Promise<UserEntity | null> {
+		return await this.pqDataSource.getByEmail(email);
+	}
+
+	async edit(editDto: EditUserDto): Promise<UserEntity> {
+		const user = await this.pqDataSource.edit(editDto);
+		await this.redisDataSource.invalidateUserCache(user.id);
+		return user;
+	}
+}
