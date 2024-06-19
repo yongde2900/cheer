107ff8c22f45917dc996ed24031cdd4e567e6e04
yongde <com414141@gmail.com>
2024-06-19 15:48:54 +0800

    feat: refactor GetAllUserDto class initialization
    - Add `IsString` and `ValidateNested` decorators to the `Filter` class
    - Remove `email` and `name` properties initialization from the constructor in `GetAllUserDto`
    - Create and initialize a `Filter` object in the `GetAllUserDto` constructor
    - Modify `GetAllUserDto.create` method to use the `Filter` object for instantiation

--- src/features/users/domain/dtos/getAllUser.dto.ts
+++ src/features/users/domain/dtos/getAllUser.dto.ts
@@ -1,27 +1,38 @@
-import { IsEmail, IsOptional } from 'class-validator';
+import { IsEmail, IsOptional, IsString, ValidateNested } from 'class-validator';
 import { CoreDto, PaginationDto } from '../../../shared/domain/dtos';
+
+class Filter {
+	@IsOptional()
+	@IsString()
+	public readonly name?: string;
+
+	@IsOptional()
+	@IsEmail()
+	public readonly email?: string;
+
+	constructor(name: string, email: string) {
+		this.name = name;
+		this.email = email;
+	}
+}
 
 export class GetAllUserDto extends CoreDto<GetAllUserDto> {
 	public readonly pagination: PaginationDto;
 
-	@IsOptional()
-	public readonly name?: string | undefined;
+	@ValidateNested()
+	public readonly filter: Filter;
 
-	@IsOptional()
-	@IsEmail()
-	public readonly email?: string | undefined;
-
-	constructor(pagination: PaginationDto, name: string, email: string) {
+	constructor(pagination: PaginationDto, filter: Filter) {
 		super();
 		this.pagination = pagination;
-		this.name = name;
-		this.email = email;
+		this.filter = filter;
 		this.validate(this);
 	}
 
 	static create(obj: Record<string, unknown>): GetAllUserDto {
 		const { name, email } = obj;
 		const pagination = PaginationDto.create(obj);
-		return new GetAllUserDto(pagination, name as string, email as string);
+		const filter = new Filter(name as string, email as string);
+		return new GetAllUserDto(pagination, filter);
 	}
 }
